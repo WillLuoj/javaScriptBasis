840845893b6d56772fbccbf3ea4f9adf0865b392
WillLuo <545250671@qq.com>
2017-02-27 23:04:30 +0800

    完成task3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # javaScriptBasis
 task1 demo url : https://willluoj.github.io/javaScriptBasis/html/task1.html
 task2 demo url : https://willluoj.github.io/javaScriptBasis/html/task2.html
+task3 demo url : https://willluoj.github.io/javaScriptBasis/html/task3.html
--- javaScript/task3.js
+++ javaScript/task3.js
@@ -6,27 +6,39 @@
  * 读取id为source的列表，获取其中城市名字及城市对应的空气质量
  * 返回一个数组，格式见函数中示例
  */
-var dataUl = document.getElementById("source");
-var Li = dataUl.firstElementChild;
-var b = Li.firstElementChild;
-alert(Li.firstChild.nodeValue);
-alert(b.firstChild.nodeValue);
-//
-// function getData() {
-//     var dataUl = document.querySelector("#source");
-//
-//
-//     return data;
-//
-// }
 
+
+
+function getData() {
+    var dataUl = document.getElementById("source");
+    var li = dataUl.getElementsByTagName("li");//获取source的所有li子节点
+    var data = new Array();
+    for(var i = 0;i < li.length;i++) {
+        var b = li[i].firstElementChild;
+        var bText = b.firstChild.nodeValue;
+        var liText = li[i].firstChild.nodeValue;
+        data.push([liText,bText]);
+    }
+    return data;
+}
 /**
  * sortAqiData
  * 按空气质量对data进行从小到大的排序
  * 返回一个排序后的数组
  */
 function sortAqiData(data) {
-
+    // data = getData();
+    function compare(data1,data2) {
+        if(data1[1] > data2[1]) {
+            return 1;
+        }else if(data1[1] < data2[1]) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+    data.sort(compare);
+    return data;
 }
 
 /**
@@ -35,7 +47,14 @@ function sortAqiData(data) {
  * 格式见ul中的注释的部分
  */
 function render(data) {
-
+    // data = sortAqiData();
+    var dataHtml = "";
+    for(var i = 0;i < data.length;i++) {
+        var chineseNum = [ "一","二","三","四", "五", "六", "七", "八", "九"];
+        dataHtml += "<li>" + "第" +chineseNum[i] + "名：" + data[i][0] + "<b>" + data[i][1] + "</b>" + "</li>";
+    }
+    var render = document.querySelector("#resort");
+    render.innerHTML = dataHtml;
 }
 
 function btnHandle() {
@@ -47,7 +66,8 @@ function btnHandle() {
 function init() {
 
     // 在这下面给sort-btn绑定一个点击事件，点击时触发btnHandle函数
-
+    var but = document.getElementById("sort-btn");
+    but.onclick = btnHandle;
 }
 
 init();
