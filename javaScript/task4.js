d0fc357dcd792748c5444e799f2e5d48518cbdf4
WillLuo <545250671@qq.com>
2017-02-28 22:18:33 +0800

    添加 EventUtil对象，用于根据不同浏览器情况使用不同的时间方法

--- javaScript/task4.js
+++ javaScript/task4.js
@@ -1,5 +1,60 @@
 /**
  * Created by luojw on 2017-2-28.
  */
-var textbox = document.getElementsByClassName("input");
-
+var textbox = document.getElementById("input");
+var EventUtil = {
+    addHandler : function(element, type, handler) {
+        if(element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+    removeHandler : function(element, type, handler) {
+        if(element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+    getEvent : function(event) {
+        return event ? event : window.event;
+    },
+    getTarget : function(event) {
+        return event.target || event.srcElement;
+    },
+    preventDefault : function(event) {
+        if(event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    stopPropagation : function(event) {
+        if(event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    },
+    getCharCode : function(event) {
+        if (typeof event.charCode == "number" ) {
+            return event.charCode;
+        } else {
+            return event.keyCode;
+        }
+    }
+};
+EventUtil.addHandler(textbox, "keypress", function(event) {
+    event = EventUtil.getEvent(event);
+    var target = EventUtil.getTarget(event);
+    var charCode = EventUtil.getCharCode(event);
+    if(!/\d/.test(String.fromCharCode(charCode)) && charCode > 9 && !event.ctrlKey) {
+        EventUtil.preventDefault(event);
+        alert("请输入数字");
+    }
+});
