d4e4dec34d4ef63a5bd1fa3d0e2eb0ae30165a26
willLuo <545250671@qq.com>
2017-03-10 11:32:35 +0800

    完成字符串分割成数组

--- javaScript/task6.js
+++ javaScript/task6.js
@@ -3,36 +3,66 @@
  */
 var textbox = document.getElementById("input"),
     inputData,
-    dataArr;
+    dataArr,
+    keyCode;
 
 //事件对象
 var EventUtil = {
+    //根据浏览器对象来使用不同的方法添加事件
     addHandler : function(element, type, handler) {
         if (element.addEventListener) {
-            element.addEventListener(type, handler, false);
+            element.addEventListener(type, handler, true);//dom2级事件处理,在冒泡阶段捕获
         } else if (element.attachEvent) {
-            element.attachEvent("on" + type, handler);
+            element.attachEvent("on" + type, handler);//ie事件处理
         } else {
-            element["on" + type] = handler;
+            element["on" + type] = handler;//dom0级事件处理
         }
     },
 
-//    获取事件的event对象
+    //取消事件默认行为
+    preventDefault : function (event) {
+       if (event.preventDefault) {
+           event.preventDefault();
+       }  else {
+           event.returnValue = false;
+       }
+    },
+
+    //取得event事件对象
     getEvent : function(event) {
         return event ? event : window.event;
-    }
+    },
+
+    //取得输入的字符编码
+    getCharCode : function(event) {
+        return event.keyCode;
+
+    },
+    //使tab键输出在textarea中
+    inputTab : function(event) {
+        keyCode = EventUtil.getCharCode(event);
+        alert("输入tab");
+        EventUtil.preventDefault(event);
+        if (keyCode == 9) {
+            EventUtil.preventDefault(event);
+            alert("输入tab");
+            inputData += "  ";
+        }
+    },
+
 };
 
 
 
 
-// 输入数据处理方法块 \r,，、\s\t
+// 输入数据处理方法块 [^a-zA-Z\d\u4e00-\u9fa5,.!?()，．；;？]
 var dataHandler = {
+
 
     // 将数据分割成数组
     splitData : function() {
         inputData = textbox.value;
-        dataArr = inputData.split(/[\n\r,，、\s\t]/);
+        dataArr = inputData.split(/[^a-zA-Z\d\u4e00-\u9fa5]/);
         alert(dataArr);
 
     }
@@ -41,5 +71,9 @@ var dataHandler = {
 var test = document.querySelector(".left-input");
 EventUtil.addHandler(test, "click", function () {
 
+
     dataHandler.splitData();
-})
+});
+EventUtil.addHandler(textbox, "keyon", function () {
+    EventUtil.inputTab(EventUtil.getEvent());
+});
